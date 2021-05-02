4c80ec8067d92e6e540b29d92561a4f0d14eb879
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-05-02 20:04:47 +0900

    change fripflop

--- testbench_ff.v
+++ testbench_ff.v
@@ -1,15 +1,18 @@
 module testbench_fli;
 
-    parameter CYCLE = 10; // 1サイクルが10ユニットという意味
-    parameter HALF_CYCLE = 5;
-    parameter DLY = 5;
+    parameter CYCLE = 30; // 1サイクルが10ユニットという意味
+    parameter HALF_CYCLE = 15;
+    parameter DLY = 15;
 
     reg CK, RB; //値を保持する必要があるもの
+    wire Q;
     wire D;
 
     flipflop_not flipflop_not_0(
+    .D(D),
     .CK(CK),
-    .RB(RB)
+    .RB(RB),
+    .Q(Q)
 );
 
 // 下は常に繰り返すという意味
@@ -22,12 +25,21 @@ end
 
 // initialはプログラム開始後に一度だけ実行する、入力を表すもの
 // 今回は１０ユニットだけ遅延させた後、１０００ユニット実行する
+integer i;
 initial begin
-    #10
+//    D<=1'b0;
+    RB<=1'b1;
+    #5
     RB=1'b0;
-
+    #46
+    RB<=1'b1;
+    #5
+    RB=1'b0;
+    #46
+    
     #1000
     $finish;
 end
 
 endmodule
+
